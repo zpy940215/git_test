9aaa62035961bc28b1c2ba417315fd197a6b13c5
zpy_glb <zpy_glb@qq.com>
2019-08-24 20:16:05 +0800

    222

--- src/main/java/com/pro/git/Test.java
+++ src/main/java/com/pro/git/Test.java
@@ -1,8 +1,12 @@
 package com.pro.git;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
 	}
 }
