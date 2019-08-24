a1a0c9cb390ce1ce578b57fe1d23a95694e3b204
zpy_glb <zpy_glb@qq.com>
2019-08-24 21:08:43 +0800

    sssss

--- src/main/java/com/pro/git/Test.java
+++ src/main/java/com/pro/git/Test.java
@@ -8,5 +8,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+		System.out.println("123");
 	}
 }
